915f162d834dd37249ffffb6bd9cc699ec974059
frankie-yanfeng <frankie-yanfeng@gmail.com>
2019-03-01 22:58:07 +0800

    function pointer

---
+++ C++/Lesson2/funcPointer1/cmake-build-debug/CMakeFiles/funcPointer1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/funcPointer1.dir/main.cpp.o"
+  "funcPointer1.pdb"
+  "funcPointer1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/funcPointer1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C++/Lesson2/funcPointer2/cmake-build-debug/CMakeFiles/funcPointer2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/funcPointer2.dir/main.cpp.o"
+  "funcPointer2.pdb"
+  "funcPointer2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/funcPointer2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
