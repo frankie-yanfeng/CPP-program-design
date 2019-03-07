f67a31f879f4124b9fa02c3ba710e6fe6c96d052
frankie-yanfeng <frankie-yanfeng@gmail.com>
2019-03-07 22:45:42 +0800

    class template Inheriance

---
+++ C++/Lesson7/classTemplateInheriance/cmake-build-debug/CMakeFiles/classTemplateInheriance.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/classTemplateInheriance.dir/main.cpp.o"
+  "classTemplateInheriance.pdb"
+  "classTemplateInheriance"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/classTemplateInheriance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
